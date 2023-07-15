7b6a9a3b713a4cefb198cd1404619740c3817d31
GhulamAli <10416351+ali38241@users.noreply.github.com>
2023-07-15 21:42:41 +0500

    Initial commit

---
+++ src/main/java/com/MFSYS/DB/Monitoring/DbMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.MFSYS.DB.Monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MFSYS/DB/Monitoring/DbMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MFSYS.DB.Monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
